86b20d211365cf1f4f831eb68ac79449ecc46660
Omer-Cicek <omercicek160408@gmail.com>
2023-08-21 00:34:10 +0300

    private router added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAz5EdZbjt8EfRCvMdApr7qtJZtLdHpUU8',
+  authDomain: 'instagram-clone-1233c.firebaseapp.com',
+  projectId: 'instagram-clone-1233c',
+  storageBucket: 'instagram-clone-1233c.appspot.com',
+  messagingSenderId: '350824176252',
+  appId: '1:350824176252:web:1519a411c0cdcf043d07f9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import PrivateRoute from 'components/PrivateRoute';
+import Home from 'pages/Home';
+import Login from 'pages/Login';
+import AuthLayout from 'pages/auth';
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />,
+    auth: true,
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [{ path: 'login', element: <Login /> }],
+  },
+];
+
+const authCheck = (routes) =>
+  routes.map((route) => {
+    if (route?.auth) {
+      route.element = <PrivateRoute>{route.element}</PrivateRoute>;
+    }
+    if (route?.children) {
+      route.children = authCheck(route.children);
+    }
+  });
+
+export default routes;
